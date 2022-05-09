7c4ed95a0718ca84d66464e0999605d628db1e88
songkeunil <jsmn114@gmail.com>
2022-05-09 11:08:54 +0900

    이거저거

--- src/main/java/com/djplat/project/board/controller/BoardController.java
+++ src/main/java/com/djplat/project/board/controller/BoardController.java
@@ -3,30 +3,11 @@ package com.djplat.project.board.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-<<<<<<< HEAD
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-
-@Controller
-public class BoardController {
-//	ModelAndView boardList(HttpServletRequest request, HttpServletResponse response) throws Exception;
-	@RequestMapping("/board.html")
-	public String boardList(Model model) {
-		return "boardList";
-	}
-
-}
-	
-=======
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
+
 
 public interface BoardController {
 
@@ -42,4 +23,3 @@ public interface BoardController {
 	
 
 }
->>>>>>> 1a546691634b551650a3603a8362342537e9d123
--- src/main/java/com/djplat/project/common/interceptor/ViewNameInterceptor.java
+++ src/main/java/com/djplat/project/common/interceptor/ViewNameInterceptor.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class ViewNameInterceptor extends HandlerInterceptorAdapter {
-<<<<<<< HEAD
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 		try {
@@ -18,59 +17,6 @@ public class ViewNameInterceptor extends HandlerInterceptorAdapter {
 		}
 		return true;
 	}
-
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-	}
-
-	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-			throws Exception {
-	}
-
-	private String getViewName(HttpServletRequest request) throws Exception {
-		String contextPath = request.getContextPath();
-		String uri = (String) request.getAttribute("javax.servlet.include.request_uri");
-		if (uri == null || uri.trim().equals("")) {
-			uri = request.getRequestURI();
-		}
-
-		int begin = 0;
-		if (!((contextPath == null) || ("".equals(contextPath)))) {
-			begin = contextPath.length();
-		}
-
-		int end;
-		if (uri.indexOf(";") != -1) {
-			end = uri.indexOf(";");
-		} else if (uri.indexOf("?") != -1) {
-			end = uri.indexOf("?");
-		} else {
-			end = uri.length();
-		}
-
-		String fileName = uri.substring(begin, end);
-		if (fileName.indexOf(".") != -1) {
-			fileName = fileName.substring(0, fileName.lastIndexOf("."));
-		}
-		if (fileName.lastIndexOf("/") != -1) {
-			fileName = fileName.substring(fileName.lastIndexOf("/", 1), fileName.length());
-		}
-		return fileName;
-	}
-}
-=======
-		@Override
-		public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-			try {
-				String viewName = getViewName(request);
-				request.setAttribute("viewName", viewName);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return true;
-		}
 
 		@Override
 		public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
@@ -113,4 +59,3 @@ public class ViewNameInterceptor extends HandlerInterceptorAdapter {
 			return fileName;
 		}
 	}
->>>>>>> 1a546691634b551650a3603a8362342537e9d123
